d779fef2aa18120551083534d71beb5bc72ebfd4
Joeybaptiste11 <59654985+Joeybaptiste11@users.noreply.github.com>
2021-01-10 03:22:04 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,18 +3,18 @@ from selenium import webdriver
 import os
 
 # Set screen resolution to 1366 x 768 like most 15" laptops
-display = Display(visible=0, size=(1366, 768))
+display = Display(visible=0, size=(800, 500))
 display.start()
 
 
 path = os.path.dirname(os.path.abspath(__file__))
-gecko_path = str(path) + "/geckodriver.exe"
+gecko_path = "/home/ubuntu/NIKE/gecko.exe"
   
 # now Firefox will run in a virtual display.
 browser = webdriver.Firefox(executable_path=gecko_path)
 
 # Sets the width and height of the current window
-browser.set_window_size(1366, 768)
+browser.set_window_size(500, 400)
 
 # Open the URL
 browser.get('https://www.vionblog.com/')
